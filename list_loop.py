f0055d874c2b62b8ed1a8a62b4a6d03dd892d61d
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2020-11-16 12:13:24 +0600

    Add files via upload
    Loop in side a list.

---
+++ list_loop.py
@@ -0,0 +1,12 @@
+"""
+
+li = list(range(11))
+print(li)
+
+li = list(range(1, 10, 2))
+print(li)
+
+"""
+saarc = ["Bangladesh", "India", "Pakistan", "Sri Lanka", "Nepal", "Bhutan", "Afghanistan"]
+for country in saarc:
+    print(country," is a member of saarc")
