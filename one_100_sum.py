ada99d6e3afb1f2bf6470598e47ceb40908ce62c
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2020-11-16 12:01:37 +0600

    Add files via upload
    *sum of 1 to 100
    *even numbers in between 1 to 100

---
+++ one_100_sum.py
@@ -0,0 +1,31 @@
+"""
+sum of 1 to 100..........................
+************using for loop*******
+result = 0
+for num in range(101):
+    result += num
+print(result)
+
+**********using while loop********
+result = 0
+i = 0
+while i < 100:
+    print(i)
+    i = i + 1
+    result = result + i
+print("sum of total: ", result)
+
+
+
+even numbers between  1 to 100..............
+for i in range(0, 101, 2):
+    print(i)
+    i += 1
+
+"""
+i = 0
+while i <= 100:
+    print(i)
+    i = i + 2
+
+
